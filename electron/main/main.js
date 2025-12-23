10b77763ae3499d58b3b89065d4096b2dd4681a7
Otavio <otavio.zerotwo@gmail.com>
2025-12-22 22:08:26 -0300

    feat: implementação create main window main.js

---
+++ electron/main/main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow, Menu } = require('electron');
+
+let mainWindow;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    roundedCorners: false,
+    webPreferences: {
+      preload: __dirname + '/preload.js'
+    }
+  });
+  
+  mainWindow.maximize();
+  mainWindow.loadURL('http://localhost:5173');
+
+  Menu.setApplicationMenu(null);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
